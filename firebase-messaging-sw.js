755e1cf10f398f9a2ff8c92ce10f777043ee427f
Nafil <pond42.grating@icloud.com>
2025-01-07 21:43:56 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,16 +1,17 @@
-// Import Firebase Scripts
-importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging.js");
+// firebase-messaging-sw.js
 
-// Firebase Configuration
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging.js");
+
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAXZgxM6PU-T3D2811KnBszblRWju7rv1Y",
     authDomain: "dws-management.firebaseapp.com",
     projectId: "dws-management",
-    storageBucket: "dws-management.appspot.com",
+    storageBucket: "dws-management.firebasestorage.app",
     messagingSenderId: "23827231923",
     appId: "1:23827231923:web:97141b6d19c7cd109d2cc6",
-    measurementId: "G-L14GLJDYW7",
+    measurementId: "G-L14GLJDYW7"
 };
 
 // Initialize Firebase
@@ -19,14 +20,16 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firebase Messaging
 const messaging = firebase.messaging();
 
-// Handle Background Notifications
+// Handle background messages
 messaging.onBackgroundMessage((payload) => {
-    console.log("Received background message:", payload);
-    const notificationTitle = payload.notification.title;
+    console.log("Received background message: ", payload);
+
+    const notificationTitle = payload.notification.title || "Default Title";
     const notificationOptions = {
-        body: payload.notification.body,
-        icon: payload.notification.icon,
+        body: payload.notification.body || "Default Body",
+        icon: payload.notification.icon || "/firebase-logo.png",
     };
 
+    // Show notification
     self.registration.showNotification(notificationTitle, notificationOptions);
 });
